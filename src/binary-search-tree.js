e1aa9cef1b5dab803f6e222c6ad701166a7b06e3
YuliAdam <ylia_adamovich@mail.ru>
2025-01-02 23:00:00 +0100

    feat: add metods add,getroot,has,find

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,63 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+
 class BinarySearchTree {
+  
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data);
+    if (!this.rootNode) this.rootNode = newNode;
+    else this.searchPosition(this.rootNode, newNode);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  searchPosition(nowNode, newNode) {
+    if (newNode.data > nowNode.data) {
+      if (!nowNode.right) {
+        nowNode.right = newNode;
+      } else {
+        this.searchPosition(nowNode.right, newNode);
+      }
+    } else {
+      if (!nowNode.left) {
+        nowNode.left = newNode;
+      } else {
+        this.searchPosition(nowNode.left, newNode);
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  searchNode( data, node) {
+    if (!node) {
+      return -1;
+    }
+    if (data === node.data) {
+      return node;
+    } else if (data > node.data) {
+      return this.searchNode( data, node.right);
+    } else {
+      return this.searchNode( data, node.left);
+    }
+  }
+  
+  has(data) {
+    return this.searchNode(data, this.rootNode) !== -1;
+  }
+
+  find(data) {
+    return this.searchNode( data, this.rootNode) ===-1 ? null : this.searchNode( data, this.rootNode);
   }
 
   remove(/* data */) {
