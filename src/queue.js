a16485fbbe849b72585682c2cb55b32a31dfb9db
YuliAdam <ylia_adamovich@mail.ru>
2025-01-03 19:13:12 +0100

    feat: add metods get, enqueue,dequeue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+ const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(){
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue( value ) {
+    let node = new ListNode(value); 
+    if(!this.head) {
+      this.head = node;
+      this.tail = node; 
+    }
+
+    this.tail.next = node;
+    this.tail = node;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this.head){
+      const el = this.head.value;
+      this.head = this.head.next;
+      if(!this.head){
+        this.tail = null; 
+      }
+      return el;
+    }
+    return null;
   }
 }
 
